fed3b6dc188bc7a5da6948b6ac6e1d6e865b94a4
Seyadashaer <114147627+Seyadashaer@users.noreply.github.com>
2022-11-03 11:16:59 +0200

    commit

--- Examples/CoinChange.js
+++ Examples/CoinChange.js
@@ -3,15 +3,37 @@ Implement generateCoinChange(cents)​ that accepts a parameter for the number o
 and computes how to represent that amount with the smallest number of coins. Console.log the result. 
 A penny is worth 1 cent. A nickel is worth 5 cents. A dime is worth 10 cents. A quarter is worth 25 cents.*/ 
 
-function generateCoinChange(cents) {
-    var penny = 1 * cents; 
-    var  nickel = 5 * cents; 
-    var dime = 10 * cents;
-    var quarter = 25 * cents; 
-    var half = 50 * cents; 
 
-    if (cents%50 == 0) {
-        half = cents/50 
+function generateCoinChange(cents) {
+    var coinChange = []; 
+    var quarters = 0; 
+    var dimes = 0; 
+    var nickels = 0; 
+    var pennies = 0; 
+    while(cents > 0) {
+        if (cents>25) {
+            cents -= 25; 
+            quarters ++;
+        } else if (cents>10) {
+            cents -= 10; 
+            dimes ++; 
+        } else if (cents > 5 ) {
+            cents -=5;
+            nickels ++;
+        } else if (cents>=1) {
+            cents -= 1; 
+            pennies ++; 
+        }
+    }
+    coinChange.push("quarters: "  + quarters);
+    coinChange.push("dimes: " + dimes);
+    coinChange.push("nickels: " + nickels);
+    coinChange.push("pennies: " + pennies);
+    return coinChange;
     }
 
-}
+console.log(generateCoinChange(167));
+
+
+
+
